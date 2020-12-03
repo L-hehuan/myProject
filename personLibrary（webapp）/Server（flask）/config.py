909571be7d7175014df721820cc283a112f56b81
L-hehuan <59555441+L-hehuan@users.noreply.github.com>
2020-12-03 09:45:08 +0800

    Update config.py

--- personLibrary（webapp）/Server（flask）/config.py
+++ personLibrary（webapp）/Server（flask）/config.py
@@ -1,5 +1,5 @@
-AccessKey_ID="LTAI4GDwZHYSscp2kmD8ywQD"
-AccessKey_Secret="862r2ip3EQ04VngYRYykBj3XCB8Jor"
+AccessKey_ID=""
+AccessKey_Secret=""
 redisConfig={
     "host":"localhost",
     "port":6379
